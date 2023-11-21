7cdd09e1ecce9d777d7ed2e188f7528a0e584740
conflabermits <christopher.dunaj@gmail.com>
2023-11-21 14:33:22 -0500

    Adding webUI command, functions, and content

--- pkg/common.go
+++ pkg/common.go
@@ -1,13 +1,12 @@
 package common
 
 import (
-	//"embed"
+	"embed"
 	"encoding/json"
 	"fmt"
-
-	//"html/template"
+	"html/template"
 	"io"
-	//"io/fs"
+	"io/fs"
 	"net/http"
 	"time"
 )
@@ -48,4 +47,40 @@ func Parse_json(jsonString string) string {
 	}
 	response = string(full_json)
 	return response
+}
+
+type ResultDetails struct {
+	Success  bool
+	URL      string
+	Response string
+}
+
+//go:embed static
+var content embed.FS
+
+func Web(port string) {
+	filesys := fs.FS(content)
+	tmpl := template.Must(template.ParseFS(filesys, "static/index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		reqURL := r.FormValue("url")
+		fmt.Println("Request URL: " + reqURL)
+		httpResponse := Http_req(reqURL)
+		response := Parse_json(httpResponse)
+		fmt.Println("Response: " + response)
+
+		result := ResultDetails{
+			Success:  true,
+			URL:      reqURL,
+			Response: response,
+		}
+		tmpl.Execute(w, result)
+	})
+
+	http.ListenAndServe(":"+port, nil)
 }
